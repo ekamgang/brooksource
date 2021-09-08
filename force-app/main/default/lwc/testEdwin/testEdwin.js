51bce93081f72bb94e3d212173e3eff6d95e3ed6
Ekamgan1 <edwin.kamgang1@t-mobile.com>
2021-09-08 11:26:24 -0400

    Fourth commit

--- force-app/main/default/lwc/testEdwin/testEdwin.js
+++ force-app/main/default/lwc/testEdwin/testEdwin.js
@@ -1,12 +1,12 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import CONTACT_OBJECT from '@salesforce/schema/Contact';
-import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
-import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
-import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
-export default class ContactCreator extends LightningElement {
-    objectApiName = CONTACT_OBJECT;
-    fields = [FIRSTNAME_FIELD, LASTNAME_FIELD, EMAIL_FIELD];
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+export default class AccountCreator extends LightningElement {
+    objectApiName = ACCOUNT_OBJECT;
+    fields = [NAME_FIELD, REVENUE_FIELD, INDUSTRY_FIELD];
     handleSuccess(event) {
         const toastEvent = new ShowToastEvent({
             title: "Account created",
